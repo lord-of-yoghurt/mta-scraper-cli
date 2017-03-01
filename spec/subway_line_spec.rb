ab292884430f3d7880c59ce39392dd30ca7d942d
David Ordovskiy <david.ordovsky@gmail.com>
2017-03-01 12:17:58 -0500

    add a couple more tests for SubwayLine class

--- spec/subway_line_spec.rb
+++ spec/subway_line_spec.rb
@@ -7,5 +7,19 @@ describe SubwayLine do
 		expect(test_line.name).to eq('1 2 3')
 		expect(test_line.status).to eq('Planned Work')
 	end
-	
+
+	context 'with a link to details on service changes' do
+		it 'allows links to be added later on' do
+			test_line.info_link = 'http://assistive.usablenet.com/tt/www.mta.info/status/subway/some_line/some_page'
+
+			expect(test_line.info_link).not_to be_nil
+		end
+	end
+
+	context 'with array to store all lines' do
+		it 'stores instances in class array' do
+			expect(SubwayLine.all).not_to be_empty
+		end
+	end
+
 end
