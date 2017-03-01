33d960681d7b4e2d903c3c9fda753ed325cbb606
David Ordovskiy <david.ordovsky@gmail.com>
2017-02-28 19:19:38 -0500

    add functionality to parse service status into a hash

--- lib/service_scraper.rb
+++ lib/service_scraper.rb
@@ -2,8 +2,34 @@ class ServiceScraper
 
 	URL = 'http://assistive.usablenet.com/tt/www.mta.info/?un_jtt_v_status=subwayTab'
 
-	def self.get_all_info
+	def self.get_page
 		Nokogiri::HTML(open(URL))
 	end
-	
+
+	def self.parse_info
+		data = self.get_page
+
+		lines = data.css('ul')[1].css('li')
+
+		info = Hash.new
+
+		lines.each do |line|
+			line_name = line.text.gsub(/ Subway.+/, '').strip
+			status = line.css('strong').text
+			next if line_name.include?('Staten') # add this back later?
+
+			info[line_name.to_sym] = status
+		end
+
+		info
+	end
+
 end
+
+# ServiceScraper.parse_info => {'1 2 3': 'Good Service', '4 5 6': 'Planned Work'}
+
+# data = Nokogiri::HTML(open('http://assistive.usablenet.com/tt/www.mta.info/?un_jtt_v_status=subwayTab'))
+# lines = data.css('ul')[1].css('li')
+# good_service = lines.select { |obj| obj.css('strong').text == 'Good Service' }
+# planned_work = lines.select { |obj| obj.css('strong').text == 'Planned Work' }
+# service_change = lines.select { |obj| obj.css('strong').text == 'Service Change' }
--- spec/service_scraper_spec.rb
+++ spec/service_scraper_spec.rb
@@ -1,13 +1,27 @@
 describe ServiceScraper do
 
-	context '#get_all_info'
+	describe '#get_page' do
 		it 'loads data from MTA\'s Subway Service page' do
-			result = ServiceScraper.get_all_info
+			result = ServiceScraper.get_page
 
 			test_element = result.css('meta').attribute('content').value
 
 			expect(test_element).not_to include('dnsrsearch')
 		end
+	end
+
+	describe '#parse_info' do
+		it 'creates a hash with line names and the service status for each line' do
+			result = ServiceScraper.parse_info
+
+			expect(result).to include('1 2 3': a_kind_of(String))
+		end
+
+		# it 'handles links properly' do
+		# 	result = ServiceScraper.parse_info
+
+		# 	expect(result).to ???
+		# end
 	end
 
 end
