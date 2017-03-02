32e551093a99d0152d5501f45c140927349c4d62
David Ordovskiy <david.ordovsky@gmail.com>
2017-03-01 19:18:31 -0500

    fix the spec for ServiceScraper

--- spec/service_scraper_spec.rb
+++ spec/service_scraper_spec.rb
@@ -13,18 +13,16 @@ describe ServiceScraper do
 	end
 
 	describe '#parse_info' do
-		it 'creates a hash with line names and the service status for each line' do
+		it 'returns an array of hashes for each subway lines' do
 			result = ServiceScraper.parse_info(test_url)
 
-			expect(result).to include(:'1 2 3' => a_kind_of(String))
+			expect(result).to include({name: '1 2 3', status: 'Good Service'})
 		end
-	end
 
-	describe '#get_service_changes' do
-		it 'creates a hash with lines that have service changes and links for further info' do
-			result = ServiceScraper.get_service_changes(test_url)
+		it 'parses links where appropriate' do
+			result = ServiceScraper.parse_info(test_url)
 
-			expect(result).to include(:'4 5 6' => 'http://assistive.usablenet.com/tt/www.mta.info/status/subway/456/24805646')
+			expect(result).to include({name: '4 5 6', status: 'Planned Work', info_link: 'http://assistive.usablenet.com/tt/www.mta.info/status/subway/456/24805646'})
 		end
 	end
 
